b283564deec09eeea60c3937a2162f83d0ff368a
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-03-26 16:20:41 -0400

    begin modulo operations

--- RingBuffer.cpp
+++ RingBuffer.cpp
@@ -4,7 +4,7 @@
 
 // ============================================================ con/destructors
 RingBuffer::RingBuffer(int capacity) :
-	_first(0), _last(0), _capacity(capacity) {
+	_first(0), _size(0), _capacity(capacity) {
     
 	_buffer.resize(_capacity);
 }
@@ -30,22 +30,31 @@ bool RingBuffer::isFull() {
 }
 
 void RingBuffer::enqueue(int16_t x) {
-	_buffer.at(_last) = x;
+	int last = (_first + _size-1) % _buffer._capacity;
+	
+/*	_buffer.at(_last) = x;
 	_last++;
 	
 	// if _last falls off the end, wrap it around
 	if (_last > _capacity)
 		_last = 0;
+*/
 }
+
 
 int16_t RingBuffer::dequeue() {
 	int16_t dequeued = _buffer.at(_first);
+	_first = (_first+1) % _buffer._capacity;
+
+/*	int16_t dequeued = _buffer.at(_first);
 	_first++;
 
 	// if _first falls off the end, wrap it around
 	if (_first > _capacity)
 		_first = 0;
 
+	return dequeued;
+*/
 	return dequeued;
 }
 
