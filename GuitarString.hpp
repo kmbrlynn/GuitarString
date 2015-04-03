614a5fab884d38677448c7c9357f69b77b2d0b13
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-03 14:22:55 -0400

    initial commit

---
+++ GuitarString.hpp
@@ -0,0 +1,16 @@
+// copyright Kim Douglas 2015
+#include <SFML/Audio.hpp>
+
+class GuitarString {
+ public:
+ 	GuitarString(double frequency);
+	GuitarString(vector<sf::Int16> init);
+ private:
+
+
+
+
+
+
+
+};
